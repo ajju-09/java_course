606c937a894269103096d832e3bdc530d1da410d
Ajju <ajinkyaawachar33@gmail.com>
2024-09-18 21:07:35 +0530

    Javva time

---
+++ src/Java_time_package.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+public class Java_time_package {
+    public static void main(String[] args) {
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+    }
+}
