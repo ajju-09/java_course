37e4a8e3104678c2ea74482cd2cc95cad9a0ccc0
Ajju <ajinkyaawachar33@gmail.com>
2024-09-18 21:01:06 +0530

    if else

---
+++ src/if_else.java
@@ -0,0 +1,14 @@
+public class if_else {
+    public static void main(String[] args)
+    {
+        int age = 18;
+        if (age >= 18)
+        {
+            System.out.println("You can drive");
+        }
+        else
+        {
+            System.out.println("You are not able to drive");
+        }
+    }
+}
