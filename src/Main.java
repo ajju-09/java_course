aae853d6e5ff59650a10b70502bc57b22f47f2de
Ajju <ajinkyaawachar33@gmail.com>
2024-09-22 17:03:39 +0530

    Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Sum of three number is :");
+        int num1 = 11;
+        int num2 = 22;
+        int num3 = 33;
+        int sum = num1 + num2 + num3;
+        double num = 3.1421739871;
+        String name = "Ajju";
+        System.out.println("The sum of three number is :"+sum);
+        System.out.println(num);
+        System.out.println(name);
+    }
+
+}
